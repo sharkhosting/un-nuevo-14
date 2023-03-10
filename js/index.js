09297b985c3217226e0a3be24d71584206d0f2b6
sharkhosting <125954332+sharkhosting@users.noreply.github.com>
2023-03-10 03:04:19 -0500

    update styles and js properties

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ ScrollReveal ({
   duration: 2500,
   delay: 400
 });
+
 //Target elements, and specify options to create reveal animations
 ScrollReveal().reveal('.main-title, .section-title', { delay: 500, origin: 'left' });
 ScrollReveal().reveal('.sec-01 .image, .info', { delay: 600, origin: 'bottom' });
@@ -12,7 +13,8 @@ ScrollReveal().reveal('.text-box', { delay: 700, origin: 'right' });
 ScrollReveal().reveal('.media-icons i', { delay: 500, origin: 'bottom', interval: 200 });
 ScrollReveal().reveal('.sec-02 .image, .sec-03 .image', { delay: 500, origin: 'top' });
 ScrollReveal().reveal('.media-info li', { delay: 500, origin: 'left', interval: 200 });
+
 //Set timeout preloader
 setTimeout (function() {
   $ ('.preloader-bg').fadeToggle();
-}, 1500);
+}, 2000);
